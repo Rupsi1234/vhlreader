fed26f51039b5d1950b46663d66ba3ac3eac3dd4
Rupsi Mehta <rupsi.mehta@comprotechnologies.com>
2022-09-16 16:20:49 +0530

    commit

---
+++ test/pageobjects/login.page.js
@@ -0,0 +1,42 @@
+
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage {
+    /**
+     * define selectors using getter methods
+     */
+    get inputUsername () {
+        return $('#user_session_username');
+    }
+
+    get inputPassword () {
+        return $('#user_session_password');
+    }
+
+    get btnSubmit () {
+        return $('input[type="submit"]');
+    }
+
+    async login (username, password) {
+        console.log(username)
+        await this.inputUsername.waitForDisplayed({ timeout: 50000 });
+        await this.inputUsername.setValue(username);
+        await this.inputPassword.setValue(password);
+        await this.btnSubmit.click();
+    }
+
+    /**
+     * overwrite specific options to adapt it to page object
+     */
+     async  open (path) {
+        await console.log(path)
+      
+         await browser.url(path)
+         await browser.pause(50000)
+        
+    }
+}
+
+module.exports = new LoginPage();
---
+++ test/specs/lti.test.js
@@ -0,0 +1,16 @@
+const LoginPage = require('../pageobjects/login.page');
+const HeaderPage = require('../pageobjects/header.page');
+const fs = require('fs');
+const argv = require('yargs').argv
+describe('VHl Reader', () => {
+    it('VHL Reader Test', async () => {
+        testdata = await fs.readFileSync('./testdata/' + argv.env + '.json')
+        testjson = JSON.parse(testdata)
+        await LoginPage.open(testjson.readerurl);
+        await browser.pause(10000)
+        await LoginPage.login(argv.userName, argv.password);
+        await expect(HeaderPage.headerPage).toBeExisting();
+    });
+});
+
+
